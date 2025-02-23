6b8a2ebb0a8cf437d1f16d77ae0005515ff0338a
Carlos Alberto Martinez Palomo <martinez.palomo@outlook.com>
2025-02-23 05:58:46 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pinakas',
+  webDir: 'www'
+};
+
+export default config;
